56f4755c4cd1a2480a7e1048ec27896982ee2fef
vicboy123 <victorsmith12443562@gmail.com>
2025-06-14 12:19:06 +0200

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -3,8 +3,7 @@
 export default function handler(req, res) {
   const min = 1.00;
   const max = 5.00;
-
-  // Generate a random number between 1.00 and 5.00
   const prediction = (Math.random() * (max - min) + min).toFixed(2);
+
   res.status(200).json({ prediction });
 }
